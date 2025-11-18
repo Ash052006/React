c6ba53b4841609c0211e9d6f5fd9b42b7d60e19b
Ash052006 <aishwarysaxena2006@gmail.com>
2025-11-18 20:15:30 +0530

    02-components-done

---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+        <h1 className='card'>
+            <h1>hello react</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, aliquid!</p>
+        </h1> 
+    </div>
+  )
+}
+
+export default card
